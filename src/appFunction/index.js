eddec18d793425a529c9f45f184853969a0b67b2
Sam Goldstein <sam@stackery.io>
2018-11-30 14:58:13 -0800

    Add some function code to app and canary

--- src/appFunction/index.js
+++ src/appFunction/index.js
@@ -1,5 +1,12 @@
 exports.handler = async message => {
   console.log(message);
+  const response = {
+    statusCode: '200',
+    body: '<h1>Success</h1>',
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    }
+  };
 
-  return {};
-}
+  return response;
+};
--- src/canaryTest/index.js
+++ src/canaryTest/index.js
@@ -1,5 +1,37 @@
-exports.handler = async message => {
-  console.log(message);
+const aws = require('aws-sdk');
+const codedeploy = new aws.CodeDeploy({ apiVersion: '2014-10-06' });
+const lambda = new aws.Lambda({ apiVersion: '2015-03-31' });
 
-  return {};
-}
+exports.handler = async event => {
+  console.log('Entering PreTraffic Hook!');
+  console.log(JSON.stringify(event));
+
+  // Read the DeploymentId from the event payload.
+  const deploymentId = event.DeploymentId;
+  console.log(deploymentId);
+
+  // Read the LifecycleEventHookExecutionId from the event payload
+  const lifecycleEventHookExecutionId = event.LifecycleEventHookExecutionId;
+  console.log(lifecycleEventHookExecutionId);
+
+  /*
+      [Perform validation or prewarming steps here]
+    */
+  const functionParams = {
+    FunctionName: process.env.FUNCTION_NAME_VERSION, /* required */
+    ClientContext: 'STRING_VALUE',
+    InvocationType: 'RequestResponse',
+    Payload: '{"canary": true}' /* Strings will be Base-64 encoded on your behalf */
+  };
+  await lambda.invoke(functionParams).promise();
+  // Prepare the validation test results with the deploymentId and
+  // the lifecycleEventHookExecutionId for AWS CodeDeploy.
+  const params = {
+    deploymentId: deploymentId,
+    lifecycleEventHookExecutionId: lifecycleEventHookExecutionId,
+    status: 'Succeeded' // status can be 'Succeeded' or 'Failed'
+  };
+
+  // Pass AWS CodeDeploy the prepared validation test results.
+  await codedeploy.putLifecycleEventHookExecutionStatus(params).promise();
+};
